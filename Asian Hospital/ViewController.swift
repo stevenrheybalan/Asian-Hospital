c4caa156e672728bad143f67b395ff92d0b3e891
srbalan <srbalan@outlook.ph>
2018-03-19 12:06:00 +0800

    Initial Commit

---
+++ Asian Hospital/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Asian Hospital
+//
+//  Created by HopprLab on 19/03/2018.
+//  Copyright © 2018 Asian Hospital Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
