bb7a0c7b585178f22335edf136b07ae0aa4bd8c9
srbalan <srbalan@outlook.ph>
2018-04-02 11:21:00 +0800

    Add patient login view controller

---
+++ Asian Hospital/Controllers/PatientLoginController.swift
@@ -0,0 +1,24 @@
+//
+//  PatientLoginController.swift
+//  Asian Hospital
+//
+//  Created by HopprLab on 02/04/2018.
+//  Copyright © 2018 Asian Hospital Inc. All rights reserved.
+//
+
+import UIKit
+
+class PatientLoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
