ae8c229acc7d7e0cdd38b694265b411dcd4f2586
srbalan <srbalan@outlook.ph>
2018-04-04 10:46:54 +0800

    Clean the main storyboard

--- Asian Hospital/Controllers/DoctorsController.swift
+++ Asian Hospital/Controllers/DoctorsController.swift
@@ -31,7 +31,7 @@ class DoctorsController: UITableViewController {
         setupNavBar()
         getDoctors()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
