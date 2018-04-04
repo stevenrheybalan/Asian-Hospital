1781e4581681382375cdd7855c71d5a3a56db2f8
srbalan <srbalan@outlook.ph>
2018-04-04 14:27:57 +0800

    Search Controller for doctors module

--- Asian Hospital/Controllers/DoctorsController.swift
+++ Asian Hospital/Controllers/DoctorsController.swift
@@ -19,9 +19,23 @@ class DoctorsController: UITableViewController {
         }
     }
     
+    private var filteredDoctors = [Doctor]() {
+        didSet {
+            if isFiltering() {
+                dataSource.update(with: filteredDoctors)
+            }else {
+                dataSource.update(with: doctors)
+            }
+            
+            tableView.reloadData()
+        }
+    }
+    
     private lazy var dataSource: DoctorsDataSource = {
         return DoctorsDataSource(doctors: doctors)
     }()
+    
+    private let searchController = UISearchController(searchResultsController: nil)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,10 +54,13 @@ class DoctorsController: UITableViewController {
     // MARK: METHODS
     
     func setupNavBar() {
-        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
         searchController.dimsBackgroundDuringPresentation = false
         searchController.hidesNavigationBarDuringPresentation = false
-        searchController.searchBar.placeholder = "Name, Department, Specialty"
+        searchController.searchBar.placeholder = "Name, Department"
+        
+        definesPresentationContext = true
         
         if #available(iOS 11.0, *) {
             navigationItem.searchController = searchController
@@ -62,5 +79,30 @@ class DoctorsController: UITableViewController {
                 print("Error: \(error)")
             }
         }
+    }
+    
+    func searchBarIsEmpty() -> Bool {
+        return searchController.searchBar.text?.isEmpty ?? true
+    }
+    
+    func isFiltering() -> Bool {
+        return searchController.isActive && !searchBarIsEmpty()
+    }
+}
+
+extension DoctorsController: UISearchResultsUpdating {
+    // MARK: SEARCH RESULTS UPDATING DELEGATE
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearchText(searchController.searchBar.text!)
+    }
+    
+    func filterContentForSearchText(_ searchText: String, scope: String = "All") {
+        filteredDoctors = doctors.filter({ (doctor: Doctor) -> Bool in
+            return doctor.displayName.lowercased().contains(searchText.lowercased()) ||
+                    doctor.department?.lowercased().contains(searchText.lowercased()) ?? false
+        })
+        
+        tableView.reloadData()
     }
 }
