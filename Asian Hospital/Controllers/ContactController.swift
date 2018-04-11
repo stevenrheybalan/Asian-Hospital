ac4053d00e680ab43face5c39131d064f732a1fa
srbalan <srbalan@outlook.ph>
2018-04-11 15:49:13 +0800

    Contact

--- Asian Hospital/Controllers/ContactController.swift
+++ Asian Hospital/Controllers/ContactController.swift
@@ -7,44 +7,22 @@
 //
 
 import UIKit
+import SafariServices
 
 class ContactController: UITableViewController {
     
-    lazy var contacts: [Contact] = {
-        guard let contactsJSON = contactData["contacts"] as? [[String: Any]] else { return [] }
-        let contacts = contactsJSON.compactMap { Contact(json: $0) }
-        return contacts
-    }()
-    
-    lazy var dataSource: ContactDataSource = {
-        return ContactDataSource(contacts: self.contacts)
+    private lazy var dataSourceDelegate: ContactDataSourceDelegate = {
+        return ContactDataSourceDelegate(tableViewItems: Constants.contactTableViewItems)
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.dataSource = dataSource
+        tableView.dataSource = dataSourceDelegate
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    // MARK: TABLE VIEW DELEGATE
-    
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 55
-    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let contact = contacts[indexPath.row]
-        
-        if let url = contact.url {
-            UIApplication.shared.open(url, completionHandler: nil)
-        }else {
-            // TODO: Show unable to call alert
-            print("Unable to call")
-        }
     }
 }
